28e3c296c81ffb8a47ab2cf5f780d5f9bfffe56d
atycholis <attyke@gmail.com>
2022-04-07 10:25:37 -0600

    Added socket io code to "AllActivities.js" and "LoginModal.js" to send requests to B-E for activities to be displayed and to ensure username is unique. Socket event listeners are implemented and commented out in the above files.

--- src/components/ui/LoginModal.js
+++ src/components/ui/LoginModal.js
@@ -1,13 +1,23 @@
 import classes from './LoginModal.module.css'
 import React from "react"
+import { io } from "socket.io-client";
+
+let socket = io('https://localhost:3001');
 
 function LoginModal(props){
     let textInput = React.createRef();
 
     function confirmHandler(){
         props.onLogin();
-        //FOR BACKEND - username from UI
-        console.log(textInput.current.value);
+        //FOR BACKEND - login request containing username from UI
+        socket.emit('login-request', textInput.current.value);
+
+        //Not certain if this is the correct place for the response event
+        //Leaning towards no will likely have to be outside the scope of a function
+
+        /*socket.on('login-response', () =>{
+            //TO DO include output if username already exists
+        });*/
     }
 
     return(
