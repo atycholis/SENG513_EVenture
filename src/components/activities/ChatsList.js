ada52d6cbfcb729157d51d93a5b66d5eb75dc550
Joel Poirier <rushfan347@gmail.com>
2022-04-12 17:36:02 -0600

    Selectable chats

--- src/components/activities/ChatsList.js
+++ src/components/activities/ChatsList.js
@@ -1,27 +1,36 @@
 import ChatPreview from './ChatPreview.js'
 import Card from '../ui/ChatCard.js'
-
-function setActive(chatid)
-{
-    
-
-}
+import React, { useEffect, useState } from 'react'
+import OpenChat from "./OpenChat";
+import Msg from "./Msg";
 
 function ChatsList(props){
+    const [activeChat, setActiveChat] = useState(props.chats.at(0).i);
+    function setActive(chatid)
+    {
+        console.log(chatid);
+        props.chats.at(activeChat).active = "false";
+        props.chats.at(chatid).active = "true";
+        setActiveChat(chatid);
+    }
     return (
-        
+       
         <ul className={"ChatsList"}>
             
             {props.chats.map(chat => (
+                <div onClick={() => {setActive(chat.i)}}>
                 <Card 
-                active={chat.active} onClick={() => {setActive(chat.id)}}>
+                active={chat.active}>
                 <ChatPreview
-                    sender={chat.sender}
-                    time={chat.time}
-                    message={chat.message}
+                    sender={chat.messages.at(0).sender}
+                    time={chat.messages.at(0).time}
+                    message={chat.messages.at(0).message}
                 />
                 </Card>
+                </div>
             ))}
+                <OpenChat chat={props.chats.at(activeChat).messages}/>
+                <Msg/>
         </ul>
         
     );
