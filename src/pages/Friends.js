f0600dfcc3a648718847fcf961320ca438daee96
Joel Poirier <rushfan347@gmail.com>
2022-04-12 17:36:02 -0600

    Selectable chats

--- src/pages/Friends.js
+++ src/pages/Friends.js
@@ -1,42 +1,49 @@
 import ChatsList from "../components/activities/ChatsList";
-import OpenChat from "../components/activities/OpenChat";
 import FriendsList from "../components/activities/FriendsList";
-import Msg from "../components/activities/Msg";
+import React, { useEffect, useState } from 'react'
 
 const DUMMY_CHATS_LIST = [{
-    message: "Hello this is a chat",
-    sender: "Brandon",
-    time: "6:01pm",
-    chatID: "1",
-    active: "true"
-},
-{
-    message: "I want to see Batman! This is an example of a reaaaaaaaaaaaaaally looooooong chat message. This is to test if a preview works or if it doesn't.",
-    sender: "Jed",
-    time: "3:02pm",
-    chatID: "2",
-    active: "false"
-},
-    ]
-
-    const DUMMY_CHAT = [{
+    active: "true",
+    i: "0",
+    messages: [{
         message: "Hello this is a chat",
         sender: "Brandon",
         time: "6:01pm",
         chatID: "1"
     },
-{
+    {
     message: "Hey Brandon, what's popping king?",
     sender: "Joel",
     time: "6:03pm",
     chatID: "3"
-},
-{
+    },
+    {
     message: "Not much, just chilling.",
     sender: "Brandon",
     time: "6:09pm",
     chatID: "4"
-},]
+    },]
+},
+{
+    active: "false",
+    i: "1",
+    messages: [{
+        message: "I want to see Batman! This is an example of a reaaaaaaaaaaaaaally looooooong chat message. This is to test if a preview works or if it doesn't.",
+        sender: "Jed",
+        time: "3:02pm",
+        chatID: "2"
+    },
+    {
+    message: "Ok",
+    sender: "Joel",
+    time: "3:03pm",
+    chatID: "5"
+    },
+  ]
+},
+    ]
+
+
 
     const DUMMY_FRIENDS_LIST = [{
         name: "Brandon"
@@ -50,10 +57,9 @@ function Friends(){
     return (
         <div>
             <h1>My Friends and Groups</h1>
-            <ChatsList chats={DUMMY_CHATS_LIST} />
             <FriendsList friends={DUMMY_FRIENDS_LIST} />
-            <OpenChat chat={DUMMY_CHAT}/>
-            <Msg/>
+            <ChatsList chats={DUMMY_CHATS_LIST} />
+            
         </div>
     );
 }
