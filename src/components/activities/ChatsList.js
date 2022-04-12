fc9f6b633a4ed33ed4dcd1e3d8666760f0353c1c
Joel Poirier <rushfan347@gmail.com>
2022-04-12 16:46:59 -0600

    chatcard

--- src/components/activities/ChatsList.js
+++ src/components/activities/ChatsList.js
@@ -1,13 +1,20 @@
 import ChatPreview from './ChatPreview.js'
 import Card from '../ui/ChatCard.js'
 
+function setActive(chatid)
+{
+    
+
+}
+
 function ChatsList(props){
     return (
         
         <ul className={"ChatsList"}>
             
             {props.chats.map(chat => (
-                <Card>
+                <Card 
+                active={chat.active} onClick={() => {setActive(chat.id)}}>
                 <ChatPreview
                     sender={chat.sender}
                     time={chat.time}
